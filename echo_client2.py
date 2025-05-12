94713e83b5c49842746cc6f9a8ebfda3805b3d4a
yihimin <yihimin01@gmail.com>
2025-05-12 12:27:36 +0900

    숫자 맞추기 게임

--- echo_client2.py
+++ echo_client2.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 9999))
@@ -7,11 +8,12 @@ while True:
     send_data = input("입력: ") 
     sock.sendall(bytes(send_data, "utf-8"))
 
-    if send_data == "end":
-        break
-
     recv_data = sock.recv(1024)
-    print(f"recv: {recv_data.decode()}")
+    decoded = recv_data.decode()
+    print(f"서버 응답: {decoded}")
+
+    if "축하합니다" in decoded:
+        break
 
 print("클라이언트 종료!")
 sock.close()
--- echo_server2.py
+++ echo_server2.py
@@ -1,7 +1,10 @@
 import socket
+import random
 
 #1.소켓 생성(ipv4 tcp)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+random_number = str(random.randint(10,99))
 
 #2.바인딩(빈 문자열은 모든 IP 인터페이스 허용, 포트 9999에서 수신)
 sock.bind(("" , 9999))
@@ -16,14 +19,17 @@ conn, addr = sock.accept()
 while True:
     #5.데이터 수신 (클라이언트로부터 최대 1024바이트 수신)
     read_data = conn.recv(1024)
+    decoded_data = read_data.decode()
     print("recv : {}".format(read_data.decode()))
 
-    if read_data.decode() == "end":
-        break
+    if decoded_data < random_number:
+            conn.sendall("업".encode("utf-8"))
+    elif decoded_data > random_number:
+            conn.sendall("다운".encode("utf-8"))
+    else:
+            conn.sendall("축하합니다! 종료 번호를 맞췄습니다.".encode("utf-8"))
+            break
 
-    #5-1. 데이터 송신
-    conn.sendall(read_data)
-    print("send : {}".format(read_data.decode()))
 
 #6.접속 종료 (클라이언트 연결 종료 + 서버 소켓도 종료)
 conn.close()
